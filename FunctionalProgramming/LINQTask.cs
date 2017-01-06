eaddc644c033b42adf61c6b687d398c4a24d30ad
aliciabigley <aliciabigley@gmail.com>
2017-01-06 09:10:04 -0600

    LINQ Remove Duplicates

---
+++ FunctionalProgramming/LINQTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class LINQTask
+    {
+        //List<string> stringList = new List<string>(); //{ "The", "The", "dog", "runs", "fast" };
+        //List<string> removeDuplicates = stringList.Distinct().ToList();     
+
+        //List<int> listOfInts = new List<int> { 1, 2, 3, 4, 1, 2, 3, 4};
+        //List<int> removeDublicates = listOfInts.Distinct().ToList();
+    }
+}
---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInts = new List<int> { 1, 2, 3, 4, 1, 2, 3, 4 };
+            List<int> removeDublicates = listOfInts.Distinct().ToList();
+
+            List<string> stringList = new List<string>(); //{ "The", "The", "dog", "runs", "fast" };
+            List<string> removeDuplicates = stringList.Distinct().ToList();
+        }
+    }
+}
