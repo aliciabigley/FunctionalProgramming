66ba22a5b6183aa8ababe9df1cbf462eeae37879
aliciabigley <aliciabigley@gmail.com>
2017-01-07 23:02:01 -0600

    Wrote method which averages student's grade after droping the lowest grade

--- FunctionalProgramming/LINQTask.cs
+++ FunctionalProgramming/LINQTask.cs
@@ -4,58 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FunctionalProgramming
 {
     public class LINQTask
     {
-        char [] nowAlphabetize;
+        //char [] nowAlphabetize;
+        List<string> removeDuplicates;
+        List<int> removeDublicates;
+        double averageGrades;
 
         public void RemoveDublicates(List<string> words, List<int> numbers)
         {
             
-            List<string> removeDuplicates = words.Distinct().ToList();
-            List<int> removeDublicates = numbers.Distinct().ToList();           
+            removeDuplicates = words.Distinct().ToList();
+            removeDublicates = numbers.Distinct().ToList();           
         }
-        public void FindAverage(List<string> grades) 
+        public double  FindAverageOneStudent(string student1)
         {
-
-            List<string> averagedGrades = grades.Average().ToList();
-
-
-    }
-
-    // public char[] FindLetterFrequency(string randomLetters)
-    //{
-    //    int[] letterFrequency = new int[(int)char.MaxValue];
-    //    foreach (char number in randomLetters)
-    //    {
-    //        letterFrequency[(int)number]++;
-    //    }
-    //        for (int i = 0; i < (int)char.MaxValue; i++)
-    //        {
-    //            if (letterFrequency[i] > 0 && char.IsLetterOrDigit((char)i))
-    //            {
-
-    //             Console.WriteLine("{0} {1}",(char)i, letterFrequency[i]);
-    //            }
-    //        }        
-    //    return nowAlphabetize;
-
-    //}
-
-    public void AlphabeticalOrder()
-        {
-            //string randomLetters = "Llewellyn";
-            //string lowerLetters = randomLetters.ToUpper();
-
-            //char[] alphabetize = lowerLetters.ToCharArray();
-            ////Array.Sort<char>(alphabetize);
-
-            //var letterCount = alphabetize.OrderBy()
-
-            //Console.WriteLine(alphabetize);
-            ////return new string(scrambledLetters);
+           var gradesStringToInt = (student1.Split(',').Select(int.Parse).ToList());
+            var averageGrades = (from i in gradesStringToInt where i > gradesStringToInt.Min() select i).Average();
+            Console.WriteLine(averageGrades);
+            this.averageGrades = averageGrades;
+            return this.averageGrades;
         }
+
+        //public void AverageGradeMultipleStudents(double this.averageGrade, int studentB)
+        //{
+        //    double[] collectStudentsGrades = { studentA, studentB };
+        //    double averageStudentGrades = collectStudentsGrades.Average();
+        //}
+
+
+        // public char[] FindLetterFrequency(string randomLetters)
+        //{
+        //    int[] letterFrequency = new int[(int)char.MaxValue];
+        //    foreach (char number in randomLetters)
+        //    {
+        //        letterFrequency[(int)number]++;
+        //    }
+        //        for (int i = 0; i < (int)char.MaxValue; i++)
+        //        {
+        //            if (letterFrequency[i] > 0 && char.IsLetterOrDigit((char)i))
+        //            {
+
+        //             Console.WriteLine("{0} {1}",(char)i, letterFrequency[i]);
+        //            }
+        //        }        
+        //    return nowAlphabetize;
+
+        //}
+
+        //public void AlphabeticalOrder()
+        //    {
+        //string randomLetters = "Llewellyn";
+        //string lowerLetters = randomLetters.ToUpper();
+
+        //char[] alphabetize = lowerLetters.ToCharArray();
+        ////Array.Sort<char>(alphabetize);
+
+        //var letterCount = alphabetize.OrderBy()
+
+        //Console.WriteLine(alphabetize);
+        ////return new string(scrambledLetters);
+        // }
         //public void TestMethod()
         //{
         //    {
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -13,11 +13,21 @@ namespace FunctionalProgramming
             LINQTask learnLINQ = new LINQTask();
             List<string> stringList = new List<string> { "The", "The", "dog", "runs", "fast" };
             List<int> listOfInts = new List<int> { 1, 2, 3, 4, 1, 2, 3, 4 };
+            string student1 = "90,100,82,89,55";
 
-            learnLINQ.RemoveDublicates(stringList,listOfInts);
+
+            //working functions:
+            learnLINQ.RemoveDublicates(stringList, listOfInts);
+            learnLINQ.FindAverageOneStudent(student1);
+            //learnLINQ.AverageGradeMultipleStudents(learnLINQ.FindAverageOneStudent, learnLINQ.FindAverageOneStudent);
+
+            //List<string> student1Grades = new List<string> { "90,100,82,89,55" };
+
+            //learnLINQ.RemoveDublicates(stringList,listOfInts); //this works
+            //learnLINQ.FindAverage(student2Grades);
             //learnLINQ.FindLetterFrequency("Llewellyn");
             //learnLINQ.AlphabeticalOrder();
-           //learnLINQ.TestMethod();
+            //learnLINQ.TestMethod();
             Console.ReadKey();
         }
     }
