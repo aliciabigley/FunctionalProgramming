c093c5592ee1d5bf12709df6a51c0fb3a40120c2
aliciabigley <aliciabigley@gmail.com>
2017-01-11 19:00:41 -0600

    Average string Split

--- FunctionalProgramming/LINQTask.cs
+++ FunctionalProgramming/LINQTask.cs
@@ -21,35 +21,38 @@ namespace FunctionalProgramming
             removeDuplicates = words.Distinct().ToList();
             removeDublicates = numbers.Distinct().ToList();           
         }
+        //string[] records = new[] { record1, record2 };
+        //string record = records.FirstOrDefault(r => r.Split(',').Any(s => s == "1"));
 
         //Problem 2
         public void AverageStudentGrades(List<string> studentGrades)
         {
             List<double> averageAverages = new List<double>();
             int gradesInt;
-
+            
             foreach (string value in studentGrades)
             {
+                var gradesToInteger = value.Split(new[] { ',' }).ToList();
                 //list of ints 
                 List<int> studentAverage = studentGrades.Select(int.Parse).ToList();
                 foreach (int x in value)
                 {
-                    int gradesInts = value.Split(new[]{ ',' }).OrderBy(a => ).Skip(1).Average();
+                    int gradesInts = studentAverage.OrderBy(s => s.value).Skip(1).Average();
+                   
                     //split at comma
                     //= gradesString.OrderBy().Skip(1).Average();
                     //return gradesInts;
                     averageAverages.Add(gradesInts);
-                    averageAverages.Average();
                 }
-               
+                Console.WriteLine(averageAverages.Average()); 
                 //Add.StudentAverage To AverageAverages.Average();
                 //.ToList();
-                List<int> gradeNumbers = new List<int>();
-                foreach (int grade in gradeNumbers)
-                {
-                    gradeNumbers.Add(Convert.ToInt32(gradeNumbers));
-                }
-                var dropMinGrade = (from i in gradeNumbers where i > gradeNumbers.Min() select i);
+                //List<int> gradeNumbers = new List<int>();
+                //foreach (int grade in gradeNumbers)
+                //{
+                //    gradeNumbers.Add(Convert.ToInt32(gradeNumbers));
+                //}
+                //var dropMinGrade = (from i in gradeNumbers where i > gradeNumbers.Min() select i);
             }
             //List<int> numberGrades = studentGrades.Select(int.Parse).ToList();
             //var gradesStringToInt = student1.Split(',').Select(int.Parse).ToList();
